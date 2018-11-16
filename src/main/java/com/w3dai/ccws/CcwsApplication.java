bc6b17dcdc841a5cdf2422edb637303912b4434f
NicolaDai <335860828@qq.com>
2018-11-16 17:27:47 +0800

    Initial commit

---
+++ src/main/java/com/w3dai/ccws/CcwsApplication.java
@@ -0,0 +1,12 @@
+package com.w3dai.ccws;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CcwsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CcwsApplication.class, args);
+    }
+}
---
+++ src/main/java/com/w3dai/ccws/HanLPTest.java
@@ -0,0 +1,9 @@
+package com.w3dai.ccws;
+
+import com.hankcs.hanlp.HanLP;
+
+public class HanLPTest {
+public static void main(String[] args){
+    System.out.println(HanLP.segment("你好，欢迎使用HanLP！"));
+}
+}
