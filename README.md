db7336e17c7fc1959526e15ad8c2d6a9cdea114b
NicolaDai <335860828@qq.com>
2018-11-19 16:59:01 +0800

    Testing code to be added.

--- README.md
+++ README.md
@@ -5,3 +5,6 @@ I felt very sad when facing with so many tools of chinese word segmentation. Whi
 
 1. when using HanLP, simply put the hanlp.properties under the "resources" directory.
 2. when adding your own dictionary, delete the txt.bin files first then run the code which will reproduce the txt.bin files by itself.
+
+# To-do List
+1. filter the customized corpus in which the repeated words are kicked out
--- src/main/java/com/w3dai/ccws/HanLPTest.java
+++ src/main/java/com/w3dai/ccws/HanLPTest.java
@@ -2,8 +2,14 @@ package com.w3dai.ccws;
 
 import com.hankcs.hanlp.HanLP;
 
+import java.io.*;
+
 public class HanLPTest {
-public static void main(String[] args){
-    System.out.println(HanLP.segment("你好，欢迎使用HanLP！"));
+public static void main(String[] args) throws IOException {
+    BufferedReader aBufferedReader = new BufferedReader(new FileReader(new File("corpus.txt")));
+    String aLine;
+    while((aLine = aBufferedReader.readLine()) != null){
+            System.out.println(HanLP.segment(aLine));
+    }
 }
 }
